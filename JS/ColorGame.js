c9b0c996862a8df382a14eac80b041276f69d88c
Neel Deorukhkar <neel.adeorukhkar@gmail.com>
2020-08-11 20:47:52 -0500

    Added functionality to change color of all the blocks to winner block at once

--- JS/ColorGame.js
+++ JS/ColorGame.js
@@ -8,8 +8,9 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -21,9 +22,24 @@ for (var i = 0; i < squares.length; i++) {
   squares[i].addEventListener("click", function () {
     var clickedColor = this.style.backgroundColor;
     if (clickedColor === pickedColor) {
-      alert("Correct");
+      messageDisplay.textContent = "Correct";
+      changeColors(clickedColor);
     } else {
-      alert("Wrong");
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again!";
     }
   });
+}
+
+function changeColors(color) {
+  // loop through all squares
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+  // change each color to match given color
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
